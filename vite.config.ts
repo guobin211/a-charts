551427919d1526beeb5d8fae470300564d91b64c
michaelbguo <michaelbguo@tencent.com>
2024-10-09 18:11:36 +0800

    feat: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+const host = process.env.TAURI_DEV_HOST
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
-  clearScreen: false,
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
+    plugins: [react()],
+    clearScreen: false,
+    server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                  protocol: 'ws',
+                  host,
+                  port: 1421
+              }
+            : undefined,
+        watch: {
+            ignored: ['**/src-tauri/**']
         }
-      : undefined,
-    watch: {
-      ignored: ["**/src-tauri/**"],
     },
-  },
-}));
+    build: {
+        target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13'
+    }
+}))
